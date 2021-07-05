49fb7ff12520b868b85c59ea7b7d26dc67bdbb12
Samet-kerim <62883356+Samet-kerim@users.noreply.github.com>
2021-07-05 21:28:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import sqlite3
+#sqlite ' ı import ettik.
+
+vt = sqlite3.connect('vtt.sqlite')
+# vtt.sqlite isimli yeni bir veritabanı dosyası oluşturalım (veya varolan bir veritabanı dosyasına bağlanalım) .
+
+
+im = vt.cursor()
+#Veritabanını oluşturduktan veya varolan bir veritabanı ile bağlantı kurduktan sonra, veritabanı üzerinde işlem yapabilmek için sonraki adımda bir imleç oluşturmamız gerekir.
+#İmleç oluşturmak için cursor() adlı bir metottan yararlanacağız.
+
+
+im.execute("""SELECT * FROM faturalar""")
+# SELECT adlı SQL komutu yardımıyla bu verileri seçtik.
+
+im.fetchone()
+#seçtiğimiz verileri tek tek almamızı sağlar.
+veriler = im.fetchall()
+#fetchall() metodu ise seçilen bu verileri alma işlevi görüyor. Yukarıda biz fetchall() metoduyla aldığımız bütün verileri veriler adlı bir değişkene atadık.
+print(veriler)
+# verileri yazdırdık.
